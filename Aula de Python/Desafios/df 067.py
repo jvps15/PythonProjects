b566c3848a0bec19b444f797fa7c8bd5cb5b192c
Juan Vinicius <juan2006.jv@gmail.com>
2024-11-15 18:45:43 -0300

    Update df 067.py

--- Aula de Python/Desafios/df 067.py
+++ Aula de Python/Desafios/df 067.py
@@ -1,9 +1,11 @@
 n = num = 0
 while True:
-    num = int(input('Digite um número que você quer ver a tabuada: ')) 
+    num = int(input('Digite um número que você quer ver a tabuada: '))
+    if num <= -1:
+        break
+    n = 0 
     while n <= 10:
         n += 1
         print(f'{n} x {num} = {n * num}')
-    if num < 0:
-        break
+print('Programa Finalizado!')
 
